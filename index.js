1ca6e95fb14d397e3b35fcfedbe9ab7e98964f24
Roxanne <reharding12@gmail.com>
2024-02-17 18:09:14 -0700

    passed 2 tests

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(array) {
+    const winningYear = array.find(obj => obj.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
